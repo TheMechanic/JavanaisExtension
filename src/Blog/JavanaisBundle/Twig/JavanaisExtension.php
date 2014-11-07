30ae64ac48a268536476d07b8123d6d8ac28cacd
TheMechanic <j.dupontbillant@gmail.com>
2014-11-07 15:01:48 +0100

    Wrap the vowels in a variable

--- src/Blog/JavanaisBundle/Twig/JavanaisExtension.php
+++ src/Blog/JavanaisBundle/Twig/JavanaisExtension.php
@@ -3,6 +3,8 @@ namespace Blog\JavanaisBundle\Twig;
 
 class JavanaisExtension extends \Twig_Extension 
 {
+    private $vowels ='aeiouAEIOUéèêëîïôöàâäùüûÉÈÊËÎÏÔÖÀÂÄÙÜÛ';
+
     public function getFilters()
     {
         return array(
@@ -16,12 +18,12 @@ class JavanaisExtension extends \Twig_Extension
         $text = lcfirst($text);
 
         // Ajoute AV entre une consonne et une voyelle
-        $pattern = '/([a-zA-Z^yYaeiouAEIOUéèêëîïôö])([aeiouAEIOUéèêëîïôö])/u';
+        $pattern = '/([a-zA-Z^yY' . $this->vowels . '])([' . $this->vowels . '])/u';
         $replacement = '$1av$2';
         $translated = preg_replace($pattern, $replacement, $text);
 
         // Ajoute AV devant les mots commençant par des voyelles
-        $pattern = '/(\s|^)([aeiouAEIOUéèêëîïôö])/u';
+        $pattern = '/(\s|^)([' . $this->vowels . '])/u';
         $replacement = '$1av$2';
         $translated = preg_replace($pattern, $replacement, $translated);
 
